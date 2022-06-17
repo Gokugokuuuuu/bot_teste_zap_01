9d7c87250cedd7f4eb4e9e13a2caf92053a41534
Gokugokuuuuu <99815220+Gokugokuuuuu@users.noreply.github.com>
2022-06-16 23:10:17 -0300

    Add files via upload

---
+++ src/menu.js
@@ -0,0 +1,34 @@
+export const menu = {
+  1: {
+    description: '01 LOGIN VPN POR 20,00 R$',
+    price: 20,
+  },
+  2: {
+    description: '10 LOGINS VPN POR 50,00 R$',
+    price: 50,
+  },
+  3: {
+    description: '30 LOGINS VPN POR 70,00 R$',
+    price: 70,
+  },
+  4: {
+    description: '40 LOGINS VPN POR 80,00 R$',
+    price: 80,
+  },
+  5: {
+    description: 'IPTV 30 DIAS POR 25,00 R$',
+    price: 25,
+ },
+  6: {
+    description: 'IPTV 60 DIAS POR 42,00 R$',
+    price: 42,
+ },
+  7: {
+    description: 'IPTV 90 DIAS POR 60,00 R$',
+    price: 60,
+ },
+  8: {
+    description: 'IPTV 12 MESES POR 120,00 R$',
+    price: 120,      
+  },
+};
